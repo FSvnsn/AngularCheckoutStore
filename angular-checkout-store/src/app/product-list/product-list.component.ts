fce5ecb803e188a76f8c32aa6a40bce9a2c71aa2
FSvnsn <fredrik.n.svendsen@gmail.com>
2019-11-12 15:38:58 +0100

    Chore: Product display

--- angular-checkout-store/src/app/product-list/product-list.component.ts
+++ angular-checkout-store/src/app/product-list/product-list.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 
-//import { products } from '../products';
-
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent {
-  //products = products;
+  products = [
+    {
+      name: "Death Stranding",
+      director: "Hideo Kojima",
+      image: "https://www.indiewire.com/wp-content/uploads/2019/10/Death-Stranding-logo.jpg?w=780"
+    },
+    {
+      name: "God of War",
+      director: "Cory Barlog",
+      image: "https://cdn.gamer-network.net/2017/usgamer/God-of-War-2018-Header-01.jpg/EG11/thumbnail/1920x1080/format/jpg/quality/75/12-12-2018-god-of-war-walkthrough-guide-beginners-guide.jpg"
+    }
+  ];
 }
 
 
